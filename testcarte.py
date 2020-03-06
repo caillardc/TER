4e0131e5756c0a40dac0eb5f72c02a5480f17f8f
caillardc <50209833+caillardc@users.noreply.github.com>
2020-03-06 19:00:46 +0100

    Update testcarte.py

--- testcarte.py
+++ testcarte.py
@@ -12,18 +12,27 @@ import folium
 # g1.add_child(folium.Marker([0,0]))
 # g2.add_child(folium.Marker([0,1]))
 # folium.LayerControl(collapsed=False).add_to(m)
-# folium.plugins.MeasureControl().add_to(m)
+# # folium.plugins.MeasureControl().add_to(m)
 # m.save('map.html')
 
 import urllib.request as urlr
 import json
 import pandas
-url = "https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-bus-vehicules-position-tr&rows=450&facet=numerobus&facet=nomcourtligne&facet=sens&facet=destination"
+url = "https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-bus-vehicules-position-tr&rows" \
+      "=450&facet=numerobus&facet=nomcourtligne&facet=destination"
 basededonnee = json.load(urlr.urlopen(url))
 
+d_lignes = {}
 df = pandas.DataFrame([basededonnee['records'][i]['fields'] for i in range(len(basededonnee['records']))])
 m = folium.Map(location = [48.146952, -1.705877])
-print(df.head(10))
+
+
 for ligne in range(len(df)):
-    folium.Marker(df.loc[ligne,'coordonnees'], name=df.loc[ligne, "idbus"]).add_to(m)
+    malignedeb = df.loc[ligne, 'nomcourtligne']
+    if malignedeb not in d_lignes.keys():
+        d_lignes[str(malignedeb)] = folium.FeatureGroup(malignedeb)
+        d_lignes[str(malignedeb)].add_to(m)
+    folium.Marker(df.loc[ligne,'coordonnees'], popup=str(df.loc[ligne, "idbus"])+ str(df.loc[ligne,'destination'])
+                  ,icon=folium.Icon(icon='bus', prefix="fa")).add_to(d_lignes[str(malignedeb)])
+folium.LayerControl(collapsed=False).add_to(m)
 m.save('map.html')
