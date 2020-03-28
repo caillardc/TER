17527f16ccce3bd010eeaee315530f04ab724d82
Pauline <38728498+HoshigakiSame@users.noreply.github.com>
2020-03-28 17:26:46 +0100

    Update TestPopo.py

--- TestPopo.py
+++ TestPopo.py
@@ -1,5 +1,7 @@
 import folium
 
-m = folium.Map(location=[45.5236, -122.6750])
+m = folium.Map(location=[48.103098,-1.6716781], zoom_start=13)
+folium.Marker(location=[48.103098,-1.6716781], icon=folium.Icon(icon="train", prefix="fa"), popup='Gare de Rennes').add_to(m)
+folium.Marker(location=[48.105439,-1.6777709], icon=folium.Icon(color="red", icon="subway", prefix="fa"), popup='Métro Charles de Gaulle').add_to(m)
 
-print(m._repr_html_())
+m.save('mapTestIcon.html')
