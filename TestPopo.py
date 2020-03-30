60c43e6e9818ca356111851c859e9a62b2a6cb76
Pauline <38728498+HoshigakiSame@users.noreply.github.com>
2020-03-30 19:15:17 +0200

    Update TestPopo.py

--- TestPopo.py
+++ TestPopo.py
@@ -5,7 +5,7 @@ m = folium.Map(location=[48.103098,-1.6716781], zoom_start=13)
 folium.Marker(location=[48.103098,-1.6716781], icon=folium.Icon(icon="train", prefix="fa"), popup='Gare de Rennes').add_to(m)
 folium.Marker(location=[48.105439,-1.6777709], icon=folium.Icon(color="red", icon="subway", prefix="fa"), popup='Métro Charles de Gaulle').add_to(m)
 
-titre = header(h1("Titre du document"))
+titre = header(h1("Titre du document", {"class" : "titre"}))
 texte = article(p("Texte explicatif")+m._repr_html_())
 body = body(titre+texte)
 page = html(title="Page 1", links_lst=[{"rel" : "stylesheet", "href" : "style1.css"}], body=body)
