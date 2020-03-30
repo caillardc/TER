fea0659b3e83ee429ac0fab7f4a239963ba328b4
Pauline <38728498+HoshigakiSame@users.noreply.github.com>
2020-03-30 18:41:56 +0200

    Update TestPopo.py

--- TestPopo.py
+++ TestPopo.py
@@ -1,14 +1,14 @@
 import folium
-import html_fct
+from html_fct import *
 
 m = folium.Map(location=[48.103098,-1.6716781], zoom_start=13)
 folium.Marker(location=[48.103098,-1.6716781], icon=folium.Icon(icon="train", prefix="fa"), popup='Gare de Rennes').add_to(m)
 folium.Marker(location=[48.105439,-1.6777709], icon=folium.Icon(color="red", icon="subway", prefix="fa"), popup='Métro Charles de Gaulle').add_to(m)
 
-titre = html_fct.header(html_fct.h1("Titre du document"))
-texte = html_fct.article(html_fct.p("Texte explicatif")+m._repr_html_())
-body = html_fct.body(titre+texte)
-page = html_fct.html(title="Page 1", links_lst=[{"rel" : "stylesheet", "href" : "style1.css"}], body=body)
+titre = header(h1("Titre du document"))
+texte = article(p("Texte explicatif")+m._repr_html_())
+body = body(titre+texte)
+page = html(title="Page 1", links_lst=[{"rel" : "stylesheet", "href" : "style1.css"}], body=body)
 
 print(page)
 f = open('page1.html','w')
